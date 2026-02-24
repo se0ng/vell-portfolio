51656b6608cf40bdf09e7379c0532f0f78539b0b
se0ng <air.05.414@gmail.com>
2026-02-24 18:59:45 +0900

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,36 @@
+const html = document.documentElement;
+const body = document.body;
+
+// open
+document.querySelectorAll('.works-item').forEach(card => {
+    card.addEventListener('click', () => {
+        const modalId = card.dataset.modal;
+        const modal = document.getElementById(modalId);
+
+        if (modal) {
+            modal.classList.add('show');
+            html.style.overflow = 'hidden';
+            body.style.overflow = 'hidden';
+        }
+    });
+});
+
+// close
+document.querySelectorAll('.modal-container').forEach(container => {
+    container.addEventListener('click', () => {
+        container.classList.remove('show');
+        html.style.overflow = '';
+        body.style.overflow = '';
+    });
+});
+
+// key Esc
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+        const activeModal = document.querySelector('.modal-container.show');
+        if (activeModal) {
+            activeModal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+    }
+});
